main_UCI - use parse_image_seg2 model - * change calculation of train/valid/test accuracy to appear before session.run * fix bug best_val_acc_ind = i-1,  minus one since valid_acc_l[0:10] don't take index 10 into account and therefore the last index is 10-1 param_manager - *  add label_encode_one_hot key parse_image_seg2 - support both normal labels and one hot encoding labels.

model.py
@@ -216,21 +216,22 @@ class NeuralNet(object):
 
                 step += 1
                 feed_dict[self.data_node_ph], feed_dict[self.tf_train_labels] = self.get_mini_batch()
-                feed_dict[self.learning_rate_ph] = self.learning_rate
-                _, l, predictions = self.sess.run(
-                    [self.optimizer, self.loss, self.prediction], feed_dict=feed_dict)
                 if (prev_epoch != self.epoch):
-                    prev_epoch = self.epoch
                     # self.logger.info('batch_labels: {}'.format(np.argmax(batch_labels, 1)))
                     # self.logger.info('predictions: {}'.format(np.argmax(predictions, 1)))
                     # self.logger.info('Minibatch loss at epoch %d: %f' % (self.epoch, l))
                     # self.logger.info('Minibatch accuracy: %.3f' % self.accuracy(predictions, batch_labels))
-                    self.logger.info('epoch %d' % self.epoch)
+                    self.logger.info('epoch %d' % prev_epoch)
                     train_acc, valid_acc, test_acc = self.eval_model()
                     train_acc_l.append(train_acc)
                     valid_acc_l.append(valid_acc)
                     test_acc_l.append(test_acc)
+                    prev_epoch = self.epoch
                     self.sess.run(self.isTrain_node.assign(True))
+
+                feed_dict[self.learning_rate_ph] = self.learning_rate
+                _, l, predictions = self.sess.run(
+                    [self.optimizer, self.loss, self.prediction], feed_dict=feed_dict)
         self.dataset.count_classes_for_all_datasets()
         #self.dataset.count_classes(batch_labels)
         self.logger.info("Training stopped at epoch: %i" % self.epoch)
@@ -312,7 +313,7 @@ class NeuralNet(object):
             val_acc_ma_l.append(val_acc_ma)
             if val_acc_ma >= best_val_acc_value:
                 best_val_acc_value = val_acc_ma
-                best_val_acc_ind = i
+                best_val_acc_ind = i-1 # minus one since valid_acc_l[0:10] don't take index 10 into account and therefore the last index is 10-1
 
         self.logger.info('Best moving average validation accuracy appeared in epoch {} and his value is: {}'.format(best_val_acc_ind, best_val_acc_value))
         self.logger.info('Test accuracy in epoch {} is: {}'.format(best_val_acc_ind, test_acc_l[best_val_acc_ind]))

parse_image_seg2.py
@@ -27,7 +27,7 @@ class Dataset(object):
         To create the class pass it the dataset_dict.
         dataset_dict = {name: 'image_segmentation', file_names: (<file name train>, <file name test>,
                         <file name validation>), assert_values_flag: <Boolean>, 'validation_train_ratio': <valid_size/train_size>
-                         'test_alldata_ratio' : <test_size / (all_dataset_size}
+                         'test_alldata_ratio' : <test_size / (all_dataset_size, 'label_encode_one_hot' : <Bool>}
         file_names can contain 1,2 or 3 file names, accordingly those files names determine the train/validation/test
         split. If only 1 file is present the split must be determined by the user.
 
@@ -87,9 +87,14 @@ class Dataset(object):
         # #update T in case we removed some features
         self.T = train_set.shape[1]
 
-        self.train_labels = self.encode_one_hot(train_labels)
-        self.validation_labels = self.encode_one_hot(validation_labels)
-        self.test_labels = self.encode_one_hot(test_labels)
+        if self.dict['label_encode_one_hot']:
+            self.train_labels = self.encode_one_hot(train_labels)
+            self.validation_labels = self.encode_one_hot(validation_labels)
+            self.test_labels = self.encode_one_hot(test_labels)
+        else:
+            self.train_labels = train_labels
+            self.validation_labels = validation_labels
+            self.test_labels = test_labels
 
         self.train_set = train_set.astype(np.float32)
         self.train_ind = np.arange(0, self.train_set.shape[0])
@@ -338,7 +343,7 @@ class Dataset(object):
         return self.num_labels
 
     def get_train_labels(self):
-        return self.train_labels[self.train_ind, :]
+        return self.train_labels[self.train_ind]
 
     def get_validation_labels(self):
         return self.validation_labels