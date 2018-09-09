f4df4d18c246652047b95f92b545b80e5c033362
uriyapes <uriyapes@gmail.com>
2018-09-09 10:24:34 +0300

    Split lenet_300_100_using_dataset.py to training part and a mnist_input_pipe.py which reads mnist data using tf.dataset.

--- lenet_300_100_using_dataset.py
+++ lenet_300_100_using_dataset.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import numpy as np
-import mnist_dataset # This doesn't contains validation dataset so use tutorials.mnist instead
-from tensorflow.examples.tutorials.mnist import input_data
+import mnist_input_pipe as dataset
 import time
 
 def weight_variable(shape):
@@ -13,66 +12,15 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 
-
-
-data_dir = './datasets/MNIST_data'
-mnist = input_data.read_data_sets(data_dir, one_hot=False)
-train_set, train_labels = mnist.train.images, mnist.train.labels
-validation_set, validation_labels = mnist.validation.images, mnist.validation.labels
-test_set, test_labels = mnist.test.images, mnist.test.labels
-train_labels = np.int32(train_labels)
-validation_labels = np.int32(validation_labels)
-test_labels = np.int32(test_labels)
-print train_labels.shape
-
 EPOCHS = 91
 batch_size = 50
 ds_seed_init = 1000
 tf.set_random_seed(10)
-# ds_train = mnist_dataset.train(data_dir)
-# ds_train = tf.data.Dataset.from_tensor_slices((train_set, train_labels)) # BAD since it consumes too much memory (see offical website)
-batch_size_ph = tf.placeholder(tf.int64)
-seed_ph = tf.placeholder(tf.int64, shape=())
-
-x, y = tf.placeholder(tf.float32, shape=[None, 784]), tf.placeholder(tf.int32, shape=[None])
-
-ds_train = tf.data.Dataset.from_tensor_slices((x, y))
-ds_train = ds_train.shuffle(buffer_size=50000, seed=seed_ph) # Shuffle before repeat to guarantee data ordering (see https://www.tensorflow.org/performance/datasets_performance)
-ds_train = ds_train.batch(batch_size_ph)
-# ds_train = ds_train.prefetch(batch_size)
-ds_train = ds_train.repeat(1)
-# iter = ds_train.make_one_shot_iterator()
-# iter = ds_train.make_initializable_iterator()
-# features, labels = iter.get_next()
-
-ds_valid = tf.data.Dataset.from_tensor_slices((x, y))
-ds_valid = ds_valid.batch(batch_size_ph)
-ds_valid = ds_valid.repeat(1) # no repeat since we want to control what happens after every epoch
-
-# ds_test = mnist_dataset.test(data_dir)
-# ds_test = tf.data.Dataset.from_tensor_slices((test_set, test_labels))
-ds_test = tf.data.Dataset.from_tensor_slices((x, y))
-ds_test = ds_test.batch(batch_size_ph)
-ds_test = ds_test.repeat(1) # no repeat since we want to control what happens after every epoch
-
-# Create reinitializable which can be initialized from multiple different Dataset objects.
-# A reinitializable iterator is defined by its structure. We could use the `output_types` and `output_shapes` properties
-#  of either `ds_train` or `ds_test` here, because they are compatible.
-iter = tf.data.Iterator.from_structure(ds_train.output_types, ds_train.output_shapes)
-features, labels = iter.get_next()
-training_init_op = iter.make_initializer(ds_train)
-valid_init_op = iter.make_initializer(ds_valid)
-test_init_op = iter.make_initializer(ds_test)
-
-
-# make a simple model
-# x = tf.placeholder("float", shape=[None, 28 * 28])
-# y_ = tf.placeholder("float", shape=[None, 10])
 
 # fc1
 W_fc1 = weight_variable([28 * 28, 300])
 b_fc1 = bias_variable([300])
-h_fc1 = tf.nn.relu(tf.matmul(features, W_fc1) + b_fc1)
+h_fc1 = tf.nn.relu(tf.matmul(dataset.get_data(), W_fc1) + b_fc1)
 
 # fc2
 W_fc2 = weight_variable([300, 100])
@@ -89,7 +37,7 @@ prediction = tf.nn.softmax(logits)
 # Define loss function, optimization technique, and accuracy metric
 # Add epsilon to prevent 0 log 0; See http://quabr.com/33712178/tensorflow-nan-bug
 # http://stackoverflow.com/questions/34240703/difference-between-tensorflow-tf-nn-softmax-and-tf-nn-softmax-cross-entropy-with
-cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels, name="cross_entropy_loss")
+cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=dataset.get_labels(), name="cross_entropy_loss")
 # "Overall, L2 regularization gives the best pruning results."
 # l2_loss = tf.nn.l2_loss(tf.concat(0, [tf.reshape(W_fc1, [-1]), tf.reshape(W_fc2, [-1]), tf.reshape(W_fc3, [-1])]))
 tf.add_to_collection('l2_loss', (tf.nn.l2_loss(W_fc1)))
@@ -103,7 +51,7 @@ loss = cross_entropy + l2_loss * l2_weight_decay
 
 train_step = tf.train.AdamOptimizer(1e-3).minimize(loss)
 # correct_prediction = tf.equal(tf.argmax(y, 1), labels)
-correct_prediction = tf.equal(tf.cast(tf.argmax(prediction, 1), tf.int32), labels)
+correct_prediction = tf.equal(tf.cast(tf.argmax(prediction, 1), tf.int32), dataset.get_labels())
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
 start_time = time.time()
@@ -112,8 +60,7 @@ with tf.Session() as sess:
     all_l1_lists = []
     for epoch in range(EPOCHS):
         # sess.run(iter.initializer, feed_dict={seed_ph: tf.cast(i*ds_seed_init, tf.int64)})
-        sess.run(training_init_op, feed_dict={seed_ph: np.int64(epoch * ds_seed_init), batch_size_ph: batch_size, # Use a predetermined seeds so that data is shuffled randomly each epoch but also it is reproducible
-                                              x: train_set, y: train_labels})
+        dataset.prepare_train_ds(sess, batch_size, np.int64(epoch * ds_seed_init))
 
         iter_num = 0
         which_labels = np.array([])
@@ -121,7 +68,7 @@ with tf.Session() as sess:
         while True: # while epoch isn't over
             iter_num += 1
             try:
-                _, acc_val, l1 = sess.run([train_step, accuracy, labels])
+                _, acc_val, l1 = sess.run([train_step, accuracy, dataset.get_labels()])
                 avg_acc += acc_val
                 if iter_num % 200 == 0:
                     print "Train batch accuracy in epoch {} in iteration {} is {}".format(epoch, iter_num, acc_val)
@@ -134,7 +81,7 @@ with tf.Session() as sess:
 
 
         # Validation
-        sess.run(valid_init_op, feed_dict={batch_size_ph: batch_size, x: validation_set, y: validation_labels})
+        dataset.prepare_validation_ds(sess, batch_size)
         iter_num = 0
         avg_acc = 0.0
         while True:         #While epoch isn't over
@@ -149,7 +96,7 @@ with tf.Session() as sess:
 
 
         # Test
-        sess.run(test_init_op,feed_dict={batch_size_ph: batch_size, x: test_set, y: test_labels})
+        dataset.prepare_test_ds(sess, batch_size)
         iter_num = 0
         avg_acc = 0.0
         while True:         #While epoch isn't over
---
+++ mnist_input_pipe.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+data_dir = './datasets/MNIST_data'
+mnist = input_data.read_data_sets(data_dir, one_hot=False)
+train_set, train_labels = mnist.train.images, mnist.train.labels
+validation_set, validation_labels = mnist.validation.images, mnist.validation.labels
+test_set, test_labels = mnist.test.images, mnist.test.labels
+train_labels = np.int32(train_labels)
+validation_labels = np.int32(validation_labels)
+test_labels = np.int32(test_labels)
+# print train_labels.shape
+
+# ds_train = tf.data.Dataset.from_tensor_slices((train_set, train_labels)) # BAD since it consumes too much memory (see offical website)
+batch_size_ph = tf.placeholder(tf.int64)
+seed_ph = tf.placeholder(tf.int64, shape=())
+
+_input_data, input_labels = tf.placeholder(tf.float32, shape=[None, 784]), tf.placeholder(tf.int32, shape=[None])
+
+ds_train = tf.data.Dataset.from_tensor_slices((_input_data, input_labels))
+ds_train = ds_train.shuffle(buffer_size=50000, seed=seed_ph) # Shuffle before repeat to guarantee data ordering (see https://www.tensorflow.org/performance/datasets_performance)
+ds_train = ds_train.batch(batch_size_ph)
+# ds_train = ds_train.prefetch(batch_size)
+ds_train = ds_train.repeat(1)
+# iter = ds_train.make_one_shot_iterator()
+# iter = ds_train.make_initializable_iterator()
+# features, labels = iter.get_next()
+
+ds_valid = tf.data.Dataset.from_tensor_slices((_input_data, input_labels))
+ds_valid = ds_valid.batch(batch_size_ph)
+ds_valid = ds_valid.repeat(1) # no repeat since we want to control what happens after every epoch
+
+# ds_test = mnist_dataset.test(data_dir)
+# ds_test = tf.data.Dataset.from_tensor_slices((test_set, test_labels))
+ds_test = tf.data.Dataset.from_tensor_slices((_input_data, input_labels))
+ds_test = ds_test.batch(batch_size_ph)
+ds_test = ds_test.repeat(1) # no repeat since we want to control what happens after every epoch
+
+# Create reinitializable which can be initialized from multiple different Dataset objects.
+# A reinitializable iterator is defined by its structure. We could use the `output_types` and `output_shapes` properties
+#  of either `ds_train` or `ds_test` here, because they are compatible.
+iter = tf.data.Iterator.from_structure(ds_train.output_types, ds_train.output_shapes)
+features, labels = iter.get_next()
+training_init_op = iter.make_initializer(ds_train)
+valid_init_op = iter.make_initializer(ds_valid)
+test_init_op = iter.make_initializer(ds_test)
+
+
+def get_data():
+    return features
+
+
+def get_labels():
+    return labels
+
+
+def prepare_train_ds(sess, batch_size, ds_seed):
+    # Use a predetermined seeds so that data is shuffled randomly each epoch but also it is reproducible
+    sess.run(training_init_op, feed_dict={seed_ph: np.int64(ds_seed), batch_size_ph: batch_size, _input_data: train_set,
+                                          input_labels: train_labels})
+
+
+def prepare_validation_ds(sess, batch_size):
+    sess.run(valid_init_op, feed_dict={batch_size_ph: batch_size, _input_data: validation_set, input_labels: validation_labels})
+
+
+def prepare_test_ds(sess, batch_size):
+    sess.run(test_init_op, feed_dict={batch_size_ph: batch_size, _input_data: test_set, input_labels: test_labels})
+
+
+
+
+
+
